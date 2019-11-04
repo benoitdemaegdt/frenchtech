d287d20acb4baf0331a01cd9864d363e31683383
benoitdemaegdt <benoit.demaegdt@gmail.com>
2019-11-04 12:15:27 +0100

    add back and front stack charts

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,81 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Analyse de la stack technique de quelques startups de la frenchtech</h1>
+    <h2>Backend</h2>
+    <bar-chart :chartData='backendData'></bar-chart>
+    <h2>Frontend</h2>
+    <bar-chart :chartData='frontendData'></bar-chart>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import companies from './data/companies.json';
+import BarChart from './components/BarChart.vue';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    BarChart,
+  },
+  data() {
+    return {
+      backendData: this.formatBackendData(),
+      frontendData: this.formatFrontendData(),
+    };
+  },
+  methods: {
+    formatBackendData() {
+      let languages = {};
+      companies.forEach((company) => {
+        languages[company.stack.backend] = (languages[company.stack.backend] || 0) + 1;
+      });
+      languages = this.sortObjectByPropertyValue(languages);
+      return {
+        labels: Object.keys(languages),
+        datasets: [
+          {
+            label: 'utilisations',
+            backgroundColor: '#f87979',
+            borderWidth: 1,
+            data: Object.values(languages),
+          },
+        ],
+      }
+    },
+    formatFrontendData() {
+      let languages = {};
+      companies.forEach((company) => {
+        languages[company.stack.frontend] = (languages[company.stack.frontend] || 0) + 1;
+      });
+      languages = this.sortObjectByPropertyValue(languages);
+      return {
+        labels: Object.keys(languages),
+        datasets: [
+          {
+            label: 'utilisations',
+            backgroundColor: '#f87979',
+            borderWidth: 1,
+            data: Object.values(languages),
+          },
+        ],
+      }
+    },
+    sortObjectByPropertyValue(inputObject) {
+      let sortedObject = {};
+      let sortedArray = [];
+      for (var key in inputObject) {
+        sortedArray.push([key, inputObject[key]]);
+      }
+      sortedArray.sort((a, b) => {
+        return b[1] - a[1];
+      });
+      sortedArray.forEach((item) => {
+        sortedObject[item[0]] = item[1];
+      });
+      return sortedObject;
+    }
   }
-}
+};
 </script>
 
 <style>
---
+++ src/components/BarChart.vue
@@ -0,0 +1,47 @@
+<script>
+import { Bar } from 'vue-chartjs'
+
+export default {
+  name: 'BarChart',
+  extends: Bar,
+  props: {
+    chartData: {
+      type: Object,
+      required: true,
+    },
+  },
+  mounted () {
+    this.renderChart(this.chartData, this.options);
+  },
+  data() {
+    return {
+      options: {
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero: true
+            },
+            gridLines: {
+              display: true
+            }
+          }],
+          xAxes: [ {
+            gridLines: {
+              display: false
+            }
+          }]
+        },
+        legend: {
+          display: false
+        },
+        responsive: true,
+        maintainAspectRatio: false
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
